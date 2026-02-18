79e3979c82efc598acc26433baa979d32da645ec
M. Mikhalychev <maxim.myhalichev@yandex.ru>
2026-02-18 23:29:02 +0300

    feat: add internal file package

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"context"
 
   "github.com/urfave/cli/v3" // imports as package "cli"
+
+  "code/internal/file"
 )
 
 func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
-		Action: func(context.Context, *cli.Command) error {
+		Action: func(_ context.Context, cmd *cli.Command) error {
+			path := cmd.Args().Get(0)
+			size, err := file.GetSize(path)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(file.FormatSize(size), path)
 			return nil
 		},
 	}
